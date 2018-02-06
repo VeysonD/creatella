ee1c8f156faa9651068b5c589cc4af781b3d4765
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-05 22:31:54 -0800

    Add React App and Index files

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>React Rendered</div>
+    )
+  }
+};
+
+export default App;

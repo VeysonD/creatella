fef81dfeed2ab09faf9758062acf9b5649a11c0a
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-05 23:10:10 -0800

    Flesh out webpack config file and add webpack script to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,29 @@ const paths = {
   PUBLIC: path.resolve(__dirname, 'public'),
   SRC: path.resolve(__dirname, 'src'),
 }
+
+module.exports = {
+  entry: path.join(paths.SRC, 'index.jsx'),
+  output: {
+    path: paths.PUBLIC,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exlude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}

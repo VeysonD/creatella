a28b4ad9fd068921a728b472fed15719557db7be
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-07 22:40:23 -0800

    Turn app into stateful component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initial: true
+    }
+  }
   render() {
     return (
       <div>React Rendered</div>
